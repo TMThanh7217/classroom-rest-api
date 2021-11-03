fa035ddd0e5646bfde2d438e332d463220eb37d1
TMThanh7217 <onthidaihocvatly12@gmail.com>
2021-11-03 14:21:28 +0700

    test remove Access-Control-Allow-Headers

--- app.js
+++ app.js
@@ -12,11 +12,11 @@ const classroomsRouter = require('./api/classrooms');
 const app = express();
 let port = process.env.PORT || 9000;
 
-app.all('/', function(req, res, next) {
+/*app.all('/', function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "X-Requested-With");
   next()
-});
+});*/
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
