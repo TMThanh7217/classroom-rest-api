f1a6df83880a718b6d013f91ad3cd0fcd58cf038
TMThanh7217 <onthidaihocvatly12@gmail.com>
2021-11-03 14:17:37 +0700

    Access-Control-Allow-Origin

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const cors = require('cors');
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
@@ -11,12 +12,19 @@ const classroomsRouter = require('./api/classrooms');
 const app = express();
 let port = process.env.PORT || 9000;
 
+app.all('/', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next()
+});
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
+app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
@@ -44,4 +52,5 @@ app.use(function(err, req, res, next) {
 app.listen(port, () => {
   console.log(`App is listening on port http://localhost:${port}`)
 });
+
 module.exports = app;
